0cd9c7c9b1941145a74d77428261eb3289523f88
Dileep Kumar <dileepkumar9030@outlook.com>
2019-07-02 02:11:47 +0530

    Fourth day

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,9 +8,9 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-//@route GET api/auth
-//@desc Test route
-//@access Public
+//@route    GET api/auth
+//@desc     Get user route
+//@access   Public
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
@@ -23,7 +23,7 @@ router.get('/', auth, async (req, res) => {
 
 //@route    POST api/auth
 //@desc     Authenticate user and get token
-//@access   Pub66666666666lic
+//@access   Public
 router.post(
   '/',
   [
