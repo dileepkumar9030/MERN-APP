3fd88122c53f468ce0523e669f91ea091efa4d31
iamdileepkumar <dileepkumar9030@outlook.com>
2020-01-13 11:44:50 +0530

    done with profile page

--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -30,6 +30,7 @@ export const getCurrentProfile = () => async dispatch => {
 //Get all profiles
 
 export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: FETCHING_DATA });
   try {
     const res = await axios.get('/api/profile');
